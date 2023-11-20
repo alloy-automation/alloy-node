e0bfaad717effcb899ee9d15879a03c107184b5f
ViralRuparel <viralruparel@gmail.com>
2023-11-21 04:15:44 +0530

    fix: url

--- utils.mjs
+++ utils.mjs
@@ -1,4 +1,4 @@
-export const baseUrl = "https://uapi-embedded.runalloy.com/2023-06";
+export const baseUrl = "https://embedded.runalloy.com/2023-06";
 export const connectionId = "";
 export const apiKey = "";
 export const shopifyConnectionId = "";
