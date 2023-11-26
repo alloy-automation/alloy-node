d90aec96594b785ac93a1d260b6182f702cb110c
Gregg Mojica <gwmojica@gmail.com>
2023-11-26 15:45:32 -0500

    api docs for the UAPI

--- utils.mjs
+++ utils.mjs
@@ -1,6 +1,6 @@
 export const baseUrl = "https://embedded.runalloy.com/2023-12";
 export const connectionId = "";
-export const apiKey = "";
+export const apiKey = "QX54M2JgYC_Q_QhJtI7i9";
 export const shopifyConnectionId = "";
 export const salesforceConnectionId = "";
 export const zohoCRMConnectionId = "";
